8d79630672930b391fe8698b04f6ff247f1c8317
Andres0918 <chablaandres@gmail.com>
2024-10-30 17:39:37 -0500

    Front bonito

--- front/src/app/pages/chat/chat.component.ts
+++ front/src/app/pages/chat/chat.component.ts
@@ -1,15 +1,22 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { EditorModule } from 'primeng/editor';
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule, EditorModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+  text: string = '';
+  messages: string[] = [];
 
+  sendMessage() {
+    if (this.text.trim()) {
+      this.messages.push(this.text); 
+      this.text = '';
+    }
+  }
 }
